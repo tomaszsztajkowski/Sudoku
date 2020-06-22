3749d4e606bae8ad5002e4334e710dddabb73c3e
tomaszsztajkowski <tomaszsztajkowski@gmail.com>
2020-06-22 13:00:41 +0200

    modified:   GUI.py 	modified:   sudoku.py

--- GUI.py
+++ GUI.py
@@ -3,107 +3,120 @@ import sudoku
 from random import randint
 
 
+
 class button():
-    def __init__(self, color, coor, size, number, outline, fontsize, locked = False, outline_size = 4):
-        self.color = color
-        self.x = 10 + (size + 2) * coor[0]
-        self.y = 10 + (size + 2) * coor[1]
-        self.width = self.height = size
-        self.number = number
-        self.outline = outline
-        self.fontsize = fontsize
-        self.locked = locked
-        self.os = outline_size
-
-    def draw(self, win):
-        #Call this method to draw the button on the screen
-        if self.outline:
-            pygame.draw.rect(win, self.outline, (self.x-self.os//2, self.y-self.os//2, self.width+self.os, self.height+self.os), 0)
-        if self.color:
-        	pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
-        
-        if self.number[0] != 0:
-            font = pygame.font.SysFont('Calibri', self.fontsize, bold= True if self.locked else False)
-            text = font.render(str(self.number[0]), 1, (0,0,0))
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2 - 1), self.y + (self.height/2 - text.get_height()/2 + 2)))
-
-    def isOver(self, pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-            
-        return False
+	def __init__(self, color, coor, size, number, locked = False):
+		self.color = color
+		self.x = 10 + (size + 2) * coor[0]
+		self.y = 10 + (size + 2) * coor[1]
+		self.size = size
+		self.number = number
+		self.locked = locked
 
 
-def display():
-	screen.fill((255, 255, 255))
-	for tile in tiles: tile.draw(screen)
-	for i in range(4): pygame.draw.line(screen, (0, 0, 0), (8, 8 + i*(tile_size+2)*3), ((tile_size+3)*9, 8 + i*(tile_size+2)*3), 4)
-	for i in range(4): pygame.draw.line(screen, (0, 0, 0), (8 + i*(tile_size+2)*3, 8), (8 + i*(tile_size+2)*3, (tile_size+3)*9), 4)
-	pygame.display.update()
+	def draw(self, win):
+		pygame.draw.rect(win, (0,0,0), (self.x-2, self.y-2, self.size+4, self.size+4), 0)
+		pygame.draw.rect(win, self.color, (self.x, self.y, self.size, self.size), 0)
+		
+		if self.number[0] != 0:
+			font = pygame.font.SysFont('Calibri', self.size - 10, bold= True if self.locked else False)
+			text = font.render(str(self.number[0]), 1, (0,0,0))
+			win.blit(text, (self.x + (self.size/2 - text.get_width()/2 - 1), self.y + (self.size/2 - text.get_height()/2 + 2)))
 
-def display_errors():
+
+	def isOver(self, pos):
+		if pos[0] > self.x and pos[0] < self.x + self.size:
+			if pos[1] > self.y and pos[1] < self.y + self.size:
+				return True
+		return False
+
+
+def display_errors(sud, tiles, chosen_tile):
 	for i in range(81):
-		if not current.validate_target(i): tiles[i].color = (255, 200, 200) 
+		if not sud.validate_target(i): tiles[i].color = (255, 200, 200) 
 		else: tiles[i].color = (255, 255, 255)
 	if chosen_tile: chosen_tile.color = (chosen_tile.color[0]- 50, chosen_tile.color[1] - 50, chosen_tile.color[2])
 
-pygame.init()
 
-with open('puzzles.txt', 'r') as f:
-	puzzles = f.read().split('\n')
-
-
-# choice = puzzles.pop(randint(0, len(puzzles)-1))
-choice = '000000000000003085001020000000507000004000100090000000500000073002010000000040009' # the worst case scenario
-# choice = '530070000600195000098000060800060003400803001700020006060000280000419005000080079'
-
-current = sudoku.Sudoku(choice)
-# solved = sudoku.Sudoku(choice)
-# solved.solve_backtracking()
-
-chosen_tile = None
-
-tile_size = 60
-tiles = []
-for i in range(9):
-	for j in range(9):
-		tiles.append(button((255, 255, 255), (j, i), tile_size, current.board[i][j], (0, 0, 0), tile_size - 10, current.board[i][j][0] != 0))
-
-
-screen = pygame.display.set_mode(((tile_size+2)*9 + 20, (tile_size+2)*9 + 20))
-pygame.display.set_caption('Sudoku')
-pygame.display.set_icon(pygame.image.load('icon.png'))
-
-run = True
-while run:
+def event_handler():
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			if event.button == 1:
-				display_errors()
-
-				mouse_pos = pygame.mouse.get_pos()
-				for i in range(81):
-					if tiles[i].locked != True and tiles[i].isOver(mouse_pos):
-						if chosen_tile: chosen_tile.color = (chosen_tile.color[0] + 50, chosen_tile.color[1] + 50, chosen_tile.color[2])
-						chosen_tile = tiles[i]
-						chosen_tile.color = (chosen_tile.color[0]- 50, chosen_tile.color[1] - 50, chosen_tile.color[2])
-						break
-
 		if event.type == pygame.KEYDOWN:
-			key = chr(event.key)
-			if chosen_tile and key in '1234567890':
-				chosen_tile.number[0] = int(key)
+			if event.key == pygame.K_EQUALS:  return 10
+			elif event.key == pygame.K_MINUS: return -10
+			return True
+	return False
 
-			if key == ' ':
-				for tile in tiles:
-					if not tile.locked: tile.number[0] = 0
-				current.solve_backtracking(functions=[display])
-			display_errors()
-    	
-	display()
+def new_sudoku(code, tile_size):
+	sud = sudoku.Sudoku(code)
+	tiles = []
+	for i in range(81):
+		tiles.append(button((255, 255, 255), (i%9, i//9), tile_size, sud.data[i], sud.data[i][0] != 0))
+	return (sud, tiles)
+
+
+
+def main():
+	def display():
+		screen.fill((255, 255, 255))
+		for tile in tiles: tile.draw(screen)
+		for i in range(4): pygame.draw.line(screen, (0, 0, 0), (8, 8 + i*(tile_size+2)*3), ((tile_size+3)*9, 8 + i*(tile_size+2)*3), 4)
+		for i in range(4): pygame.draw.line(screen, (0, 0, 0), (8 + i*(tile_size+2)*3, 8), (8 + i*(tile_size+2)*3, (tile_size+3)*9), 4)
+		pygame.display.update()
+
+	pygame.init()
+	tile_size = 60
+	screen = pygame.display.set_mode(((tile_size+2)*9 + 20, (tile_size+2)*9 + 20))
+	pygame.display.set_caption('Sudoku')
+	pygame.display.set_icon(pygame.image.load('icon.png'))
+
+
+	with open('puzzles.txt', 'r') as f:
+		puzzles = f.read().split('\n')
+
+
+	code = puzzles.pop(randint(0, len(puzzles)-1))
+	# code = '000000000000003085001020000000507000004000100090000000500000073002010000000040009' # the worst case scenario
+	# code = '530070000600195000098000060800060003400803001700020006060000280000419005000080079' # wikipedia example
+
+	board, tiles = new_sudoku(code, tile_size)
 	
+	chosen_tile = None
+	running = True
+
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					display_errors(board, tiles, chosen_tile)
+
+					mouse_pos = pygame.mouse.get_pos()
+					for i in range(81):
+						if tiles[i].locked != True and tiles[i].isOver(mouse_pos):
+							if chosen_tile: chosen_tile.color = (chosen_tile.color[0] + 50, chosen_tile.color[1] + 50, chosen_tile.color[2])
+							chosen_tile = tiles[i]
+							chosen_tile.color = (chosen_tile.color[0]- 50, chosen_tile.color[1] - 50, chosen_tile.color[2])
+							break
+
+			if event.type == pygame.KEYDOWN:
+				if chosen_tile and chr(event.key).isnumeric():
+					chosen_tile.number[0] = int(chr(event.key))
+
+				if event.key == pygame.K_SPACE:
+					for tile in tiles:
+						if not tile.locked: tile.number[0] = 0
+					chosen_tile = []
+					display_errors(board, tiles, chosen_tile)
+					board.solve_backtracking(display = display, events = event_handler)
+
+				if event.key == pygame.K_ESCAPE:
+					for tile in tiles:
+						if not tile.locked: tile.number[0] = 0
+
+				display_errors(board, tiles, chosen_tile)
+
+		display()
+	
+main()
--- sudoku.py
+++ sudoku.py
@@ -1,55 +1,59 @@
-from time import sleep
-
 sqr_indexes = {str(i // 3) + str(i % 3): i for i in range(9)}           # dictionary for square indexes keys: '00' to '22', values: 0 to 8
 
 class Sudoku:
     def __init__(self, data = None):
         if data: self.data = [[int(i)] for i in data]                               # convert string data to indexed pointers
         else: self.data = [[0] for i in range(81)]                                  # if no data is provided makes an empty board
+
         self.board = [[self.data[i*9 + j] for j in range(9)] for i in range(9)]     # 9x9 matrix of pointers from data
+
         self.board_t = list(map(list, zip(*self.board)))                            # matrix with the same pointers as in board but transposed
+
         self.board_sqr = [list() for i in range(9)]                                 # matrix with the same pointers as in board but arranged into squares like in sudoku
-        for i in range(81): self.board_sqr[sqr_indexes[str((i // 9) // 3) + str((i % 9) // 3)]].append(self.data[i])
-        
-    def is_valid(self):
-        for arrangement in [self.board, self.board_t, self.board_sqr]:  # checks every needed arrangement of numbers on board
-            for line in arrangement:
-                check = {i: False for i in range(1, 10)}                # dictionary with bool representation for appearence of numbers
-                for n in line:                                          # simple flag check
-                    if n[0] == 0: continue
-                    if check[n[0]] == True: return False
-                    else: check[n[0]] = True
-        return True
+        for i in range(81):
+            self.board_sqr[sqr_indexes[str((i//9) // 3) + str((i%9) // 3)]].append(self.data[i])
+
 
     def validate_target(self, index):
         boards = [self.board, self.board_t, self.board_sqr]                                             # regular, transposed, squares
-        indexes = [index // 9, index % 9, sqr_indexes[str((index // 9) // 3) + str((index % 9) // 3)]]  # regular, transposed, squares
+        indexes = [index//9, index%9, sqr_indexes[str((index//9) // 3) + str((index%9) // 3)]]          # regular, transposed, squares
+
         for b, i in zip(boards , indexes):
             check = {r: False for r in range(1, 10)}
+
             for n in b[i]:
                 if n[0] == 0: continue
                 if check[n[0]] == True: return False
                 else: check[n[0]] = True
+
         return True
 
-    def current_board(self):
-        return [[num[0] for num in line] for  line in self.board]       # returs flat board
 
     counter = 0
-    def solve_backtracking(self, index = 0, functions = []):
-        if self.board[index//9][index%9][0] != 0:                       # handles prefilled numbers
-            if index == 80: return True                                 # -||-
-            return self.solve_backtracking(index + 1, functions=functions)                   # -||-
+    delay = 1
+    def solve_backtracking(self, index= 0, events= None, display= None):
+        if events:
+            e = events()
+            if e == True: return True
+            elif type(e) == int:
+                self.delay += e
+                if self.delay < 1: self.delay = 1
+
+        if display:
+            if self.counter == self.delay:
+                        display()
+                        self.counter = 0
+            else: self.counter += 1
+
+        if self.board[index//9][index%9][0] != 0:                                           # handles prefilled numbers
+            if index == 80: return True                                                     # -||-
+            return self.solve_backtracking(index + 1, events=events, display=display)       # -||-
         
         while True:
-            self.board[index//9][index%9][0] += 1                       # adds 1 to the number in the current position
-            if self.board[index//9][index%9][0] > 9:                    # backtracking system, if there are no numbers left:
-                self.board[index//9][index%9][0] = 0                    #  resets previously changed numbers
-                self.counter += 1
-                if self.counter == 10:
-                    for f in functions: f()
-                    self.counter = 0
-                return False                                            #  returns wrong number flag
-            if not self.validate_target(index): continue                            # validity check
-            if index == 80: return True                                 # if reached the end of sudoku returns completion flag
-            if self.solve_backtracking(index + 1, functions=functions) == True: return True  # recursion
+            self.board[index//9][index%9][0] += 1                                           # adds 1 to the number in the current position
+            if self.board[index//9][index%9][0] > 9:                                        # backtracking system, if there are no numbers left:
+                self.board[index//9][index%9][0] = 0                                        #  resets previously changed numbers
+                return False                                                                #  returns wrong number flag
+            if not self.validate_target(index): continue                                    # validity check
+            if index == 80: return True                                                      # if reached the end of sudoku returns completion flag
+            if self.solve_backtracking(index + 1, events=events, display=display) == True: return True  # recursion
